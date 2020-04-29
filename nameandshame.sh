17651ee01eac111bbefd0a3f9caf5a7130eeb6a8
James Sawyer <sawyer12@gmail.com>
2020-04-29 22:07:01 +0100

    update 2020

--- nameandshame.sh
+++ nameandshame.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+#tip jar
+#Bitcoin Cash (BCH)  
+#qpz32c4lg7x7lnk9jg6qg7s4uavdce89myax5v5nuk
+#Ether (ETH)		    
+#0x843d3DEC2A4705BD4f45F674F641cE2D0022c9FB
+#Litecoin (LTC)			  
+#Lfk5y4F7KZa9oRxpazETwjQnHszEPvqPvu
+#Bitcoin (BTC)			    
+#14Dm7L3ABPtumPDcj3REAvs4L6w9YFRnHK
+
 #REQUIRED PARAMS (Special characters must be urlencoded.)
-username='USERNAME_HERE'
-password='PASSWORD_HERE'
+username=''
+password=''
 
 #EXTRA OPTIONS
-#uagent="Script by tg12"
+uagent="Script by tg12"
 sleeptime=0 # seconds between requests
 touch "cookie.txt" #create a temp. cookie file
 
@@ -26,59 +36,57 @@ composepage=$(curl -s -b "cookie.txt" -c "cookie.txt" -L -A "$uagent" "https://m
 
 #make the file
 grep -E '(BREAK-IN|Invalid user|Failed|refused|Illegal)' /var/log/auth.log | rev | cut -d\  -f4 | rev | sort -u > host.txt
+grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" host.txt > tmphost.txt
+mv tmphost.txt host.txt
+
 #read the file
 while read hostname
 do
-	IP=$hostname
-	REVERSE_IP=$(echo $IP | awk -F "." '{print $4"."$3"."$2"."$1}')
-	ASN_INFO=$(dig +short $REVERSE_IP.origin.asn.cymru.com TXT)
-	PEER_INFO=$(dig +short $REVERSE_IP.peer.asn.cymru.com TXT)
-	NUMBER=$(echo $ASN_INFO | cut -d'|' -f 1 | cut -d'"' -f 2 | cut -d' ' -f 1)
-	ASN="AS$NUMBER"
-	ASN_REPORT=$(dig +short $ASN.asn.cymru.com TXT)
-	SUBNET=$(echo $ASN_INFO | cut -d'|' -f 2)
-	COUNTRY=$(echo $ASN_INFO | cut -d'|' -f 3)
-	ISSUER=$(echo $ASN_INFO | cut -d'|' -f 4)
-	PEERS=$(echo $PEER_INFO | cut -d'|' -f 1 | cut -d'"' -f 2)
-	REGISTRY_DATE=$(echo $ASN_REPORT | cut -d'|' -f 4)
-	REGISTRANT=$(echo $ASN_REPORT | cut -d'|' -f 5 | cut -d'"' -f 1)
+    IP=$hostname
+    REVERSE_IP=$(echo $IP | awk -F "." '{print $4"."$3"."$2"."$1}')
+    ASN_INFO=$(dig +short $REVERSE_IP.origin.asn.cymru.com TXT)
+    PEER_INFO=$(dig +short $REVERSE_IP.peer.asn.cymru.com TXT)
+    NUMBER=$(echo $ASN_INFO | cut -d'|' -f 1 | cut -d'"' -f 2 | cut -d' ' -f 1)
+    ASN="AS$NUMBER"
+    ASN_REPORT=$(dig +short $ASN.asn.cymru.com TXT)
+    SUBNET=$(echo $ASN_INFO | cut -d'|' -f 2)
+    COUNTRY=$(echo $ASN_INFO | cut -d'|' -f 3)
+    ISSUER=$(echo $ASN_INFO | cut -d'|' -f 4)
+    PEERS=$(echo $PEER_INFO | cut -d'|' -f 1 | cut -d'"' -f 2)
+    REGISTRY_DATE=$(echo $ASN_REPORT | cut -d'|' -f 4)
+    REGISTRANT=$(echo $ASN_REPORT | cut -d'|' -f 5 | cut -d'"' -f 1)
 
-	# Print tab delimited with headers
-	#echo "#Query,Subnet,Registrant,AS Number,Country,Issuer,Registry Date,Peer ASNs"
-	#echo -e "$IP\t$SUBNET\t$REGISTRANT\t$ASN\t$COUNTRY\t$ISSUER\t$REGISTRY_DATE\t$PEERS"
-	#echo -e "$IP\t$SUBNET\t$REGISTRANT\t$ASN\t$COUNTRY\t$ISSUER"
+    # Print tab delimited with headers
+    #echo "#Query,Subnet,Registrant,AS Number,Country,Issuer,Registry Date,Peer ASNs"
+    #echo -e "$IP\t$SUBNET\t$REGISTRANT\t$ASN\t$COUNTRY\t$ISSUER\t$REGISTRY_DATE\t$PEERS"
+    #echo -e "$IP\t$SUBNET\t$REGISTRANT\t$ASN\t$COUNTRY\t$ISSUER"
 
-	temp="ATTACK FROM IP: "$IP
-	tweet="$temp  WHO:$REGISTRANT"
-	echo -e "$tweet"
+    temp="ssh brute force: "$IP
+    tweet="$temp #threatintel #malware #cybersecurity #infosec #BlueKeep #cybercrime #botnet #OSINT"
+    echo -e "$tweet"
 
-	if [ $(echo "$tweet" | wc -c) -gt 140 ]; then
-        	echo "[FAIL] Tweet must not be longer than 140 chars!" && exit 1
-	elif [ "$tweet" == "" ]; then
-        	echo "[FAIL] Nothing to tweet. Enter your text as argument." && exit 1
-	fi
+    if [ $(echo "$tweet" | wc -c) -gt 140 ]; then
+            echo "[FAIL] Tweet must not be longer than 140 chars!" && exit 1
+    elif [ "$tweet" == "" ]; then
+            echo "[FAIL] Nothing to tweet. Enter your text as argument." && exit 1
+    fi
 
-	# TWEET
-	echo "[+] Posting a new tweet: $tweet..." && sleep $sleeptime
-	tweettoken=$(echo "$composepage" | grep "authenticity_token" | sed -e 's/.*value="//' | cut -d '"' -f 1 | tail -n 1)
-	update=$(curl -s -b "cookie.txt" -c "cookie.txt" -L -A "$uagent" -d "wfa=1&authenticity_token=$tweettoken&tweet[text]=$tweet&commit=Tweet" "https://mobile.twitter.com/compose/tweet")
+    # TWEET
+    echo "[+] Posting a new tweet: $tweet..." && sleep $sleeptime
+    tweettoken=$(echo "$composepage" | grep "authenticity_token" | sed -e 's/.*value="//' | cut -d '"' -f 1 | tail -n 1)
+    update=$(curl -s -b "cookie.txt" -c "cookie.txt" -L -A "$uagent" -d "wfa=1&authenticity_token=$tweettoken&tweet[text]=$tweet&commit=Tweet" "https://mobile.twitter.com/compose/tweet")
 
 
-	#OTHER FUNCTIONALITY ADD TO DODGY ASN FILE
-	echo $ASN >> badASNs.txt
+    #OTHER FUNCTIONALITY ADD TO DODGY ASN FILE
+    echo $ASN >> badASNs.txt
 
 done < host.txt
 
-#REMOVE DUPLICATES - SORT OF!!
-#sort -u badASNs.txt
-
-#OHTHER FUNCTIONALITY
-#while read ASNTOBLOCK
-#do
-	#echo "DEBUG:" $ASNTOBLOCK
-	#ASN=$ASNTOBLOCK; for s in $(whois -H -h riswhois.ripe.net -- -F -K -i $ASN | grep -v "^$" | grep -v "^%" | awk '{ print $2 }' ); do echo "  blocking $s"; sudo iptables -A INPUT -s $s -j REJECT &> /dev/null || sudo ip6tables -A INPUT -s $s -j REJECT; done
-#done < badASNs.txt
-
+while read ASNTOBLOCK
+do
+        echo "DEBUG:" $ASNTOBLOCK
+        ASN=$ASNTOBLOCK; for s in $(whois -H -h riswhois.ripe.net -- -F -K -i $ASN | grep -v "^$" | grep -v "^%" | awk '{ print $2 }' ); do echo "  blocking $s"; sudo iptables -A INPUT -s $s -j REJECT &> /dev/null || sudo ip6tables -A INPUT -s $s -j REJECT; done
+done < badASNs.txt
 
 
 # GRAB LOGOUT TOKENS
